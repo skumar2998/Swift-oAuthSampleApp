02ce857c71b79e26737c22daf26373472462a18c
Kumar, Sunil <Sunil.Kumar2@Honeywell.com>
2018-05-27 08:40:15 +0530

    Diplaying token's using SwiftEntryKit

---
+++ oAuthSampleApp/EntryAttributeWrapper.swift
@@ -0,0 +1,16 @@
+//
+//  EntryAttributeWrapper.swift
+//  oAuthSampleApp
+//
+//  Created by Kumar, Sunil on 27/05/18.
+//  Copyright © 2018 AppScullery. All rights reserved.
+//
+
+import SwiftEntryKit
+
+class EntryAttributeWrapper {
+    var attributes: EKAttributes
+    init(with attributes: EKAttributes) {
+        self.attributes = attributes
+    }
+}
--- oAuthSampleApp/ViewController.swift
+++ oAuthSampleApp/ViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 import OAuthSwift
+import SwiftEntryKit
 
 class ViewController: UIViewController {
     
     var oauthswift: OAuthSwift?
+    
+    private lazy var attributesWrapper: EntryAttributeWrapper = {
+        var attributes = EKAttributes()
+        attributes.positionConstraints = .fullWidth
+        attributes.hapticFeedbackType = .success
+        attributes.positionConstraints.safeArea = .empty(fillSafeArea: true)
+        attributes.entryBackground = .visualEffect(style: .light)
+        attributes.position = .center
+        attributes.screenInteraction = .dismiss
+        attributes.displayDuration = .infinity
+        return EntryAttributeWrapper(with: attributes)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +37,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func oauthViaSafariButtonPressed(_ sender: UIButton) {
-        
-        
+
         let oauthswift = OAuth2Swift(
             consumerKey:    "be8889735ac2f94d7d2e8c16212635b4a9eb932f",
             consumerSecret: "11c4db6422c8cbb378bea66a0dec5e57ddef348b",
@@ -40,17 +52,30 @@ class ViewController: UIViewController {
         let _ = oauthswift.authorize(
             withCallbackURL: URL(string: "oauth-sample://oauth-callback/gitter")!, scope: "flow", state: state,
             success: { credential, response, parameters in
-                //self.showTokenAlert(name: serviceParameters["name"], credential: credential)
-                print("Gitter Token's are \r\n Key:\(credential.consumerKey) \r\n Secret:\(credential.consumerSecret)")
+                
+                self.showMessageToUser("Gitter Token's ", body: "Key:\(credential.consumerKey) \r\nSecret:\(credential.consumerSecret)")
         },
             failure: { error in
                 print(error.localizedDescription, terminator: "")
         })
+
     }
     
     func getURLHandler() -> OAuthSwiftURLHandlerType {
         return OAuthSwiftOpenURLExternally.sharedInstance
     }
     
+    
+    func showMessageToUser(_ title: String, body: String) {
+        let title = EKProperty.LabelContent(text: title, style: EKProperty.LabelStyle(font: MainFont.bold.with(size: 16), color: .black))
+        let description = EKProperty.LabelContent(text: body, style: EKProperty.LabelStyle(font: MainFont.light.with(size: 14), color: .black))
+        let image = EKProperty.ImageContent(image: UIImage(named: "ic_info_outline")!, size: CGSize(width: 30, height: 30))
+        let simpleMessage = EKSimpleMessage(image: image, title: title, description: description)
+        let notificationMessage = EKNotificationMessage(simpleMessage: simpleMessage)
+        let contentView = EKNotificationMessageView(with: notificationMessage)
+        
+        
+        SwiftEntryKit.display(entry: contentView, using: attributesWrapper.attributes)
+    }
 }
 
